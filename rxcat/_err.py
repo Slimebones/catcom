ed9b84442b5779db0c94c807766b8b0fe268a146
ryzhovalex <thed4rkof@gmail.com>
2024-07-19 12:19:26 +0300

    update test_err.py, _err.py

--- rxcat/_err.py
+++ rxcat/_err.py
@@ -1,3 +1,4 @@
+import traceback
 from typing import Self
 
 from pydantic import BaseModel
@@ -7,6 +8,9 @@ from pykit.obj import get_fully_qualified_name
 class ErrDto(BaseModel):
     """
     Represents an error as data transfer object.
+
+    "stacktrace" used to comply with other languages structures, for Python
+    it's actually a traceback.
     """
     codeid: int | None = None
     """
@@ -14,9 +18,18 @@ class ErrDto(BaseModel):
     """
     name: str
     msg: str
+    stacktrace: str | None = None
 
     @classmethod
     def create(cls, err: Exception, codeid: int | None = None) -> Self:
         name = get_fully_qualified_name(err)
         msg = ", ".join([str(a) for a in err.args])
-        return cls(codeid=codeid, msg=msg, name=name)
+        stacktrace = None
+        tb = err.__traceback__
+        if tb:
+            extracted_list = traceback.extract_tb(tb)
+            stacktrace = ""
+            for item in traceback.StackSummary.from_list(
+                    extracted_list).format():
+                stacktrace += item
+        return cls(codeid=codeid, msg=msg, name=name, stacktrace=stacktrace)
